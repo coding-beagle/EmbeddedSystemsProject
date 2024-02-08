7862e201ccd04785c2d21c5d098bac6ddc305383
The Coding Beagle <nicky23teague@gmail.com>
2024-02-08 22:55:22 +0000

    updated ble

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
-#include "mbed.h"
+#include <mbed.h>
 
-DigitalOut myled(LED1);
+Serial hm10(D1, D0); //UART6 TX,RX
+
+DigitalOut led(D5);
+DigitalOut led2(D9);
+
+char c; //the character we want to receive
 
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+
+    hm10.baud(9600);
+
+    led=1;
+    led2=1;
+
+    while(1) {
+        if(hm10.readable()){
+            led2=0;
+            c = hm10.getc(); //read a single character
+            if(c == 'A'){
+                led = 1;
+            }
+            else if(c == 'B'){
+                led = 0;
+            }
+        }
+        else{
+            led2=1;
+        }
+    }
 }
