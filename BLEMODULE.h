bb1c8a0e4baff72cbb82212ead9b2bd22d711683
The Coding Beagle <87939112+coding-beagle@users.noreply.github.com>
2024-02-27 23:13:59 +0000

    Update BLEMODULE.h

--- BLEMODULE.h
+++ BLEMODULE.h
@@ -13,55 +13,69 @@ class HM10{
                 int signal;
                 Callback<void()> callback;
                 Callback<void(int)> callback_int;
+                Callback<void(float)> callback_float;
                 bool inUse;
-                bool takesArg;
+                bool takesInt;
+                bool takesFloat;
             };
-        
-        void init();
-        
+                
         Serial bleModule;
 
         int input;
 
-        char outputBuffer[50];  // to send signals to bluetooth receiver
-        int outputBufferOutIndex;
-        int outputBufferInIndex;
-
         callbacks callbacksArray[10]; // max of ten callbacks i guess
+
+        void init();
+
+        /* Decode 4 incoming chars into a single float                */
+        float incoming_to_float();
 
     public:
 
-    HM10(PinName rx, PinName tx);
-    HM10(PinName rx, PinName tx, int baud);
+        HM10(PinName rx, PinName tx);
+        HM10(PinName rx, PinName tx, int baud);
 
-    /*  Change the Baud Rate that the HM10 Module is running on 
-        Can only be one of the specified Baud rates above
-        Returns: 0 on successful change,
-                -1 if failed                                      */
-    int setBaud(int baud);
+        /*  Change the Baud Rate that the HM10 Module is running on 
+            Can only be one of the specified Baud rates above
+            Returns: 0 on successful change,
+                    -1 if failed                                      */
+        int setBaud(int baud);
 
-    /*  Assign a callback to run when the signal number is sent
-        through BLE. 
-        Returns: 0 if the callback is successfully added
-                -1 if not (i.e. all callback slots used)          */
-    int addCallback(int signal, Callback<void()> cb);
-    int addCallback(int signal, Callback<void(int)> cb);
-    
-    /*  Remove a callback by passing in the signal that is supposed
-        to trigger it.
-        Returns: 0 if the callback is successfully removed
-                -1 if not, i.e. signal doesn't exist              */
-    int removeCallback(int signal);
+        /*  Assign a callback to run when the signal number is sent
+            through BLE. 
+            Returns: 0 if the callback is successfully added
+                    -1 if not (i.e. all callback slots used)          */
+        int addCallback(int signal, Callback<void()> cb);
+        int addCallback(int signal, Callback<void(int)> cb);
+        int addCallback(int signal, Callback<void(float)> cb);
 
-    /*  Add data to the output transmission buffer, which gets sent
-        over the BLE connection.
-        Returns: 0 on successful addition  
-        NOT WORKING YET                                           */
-    int transmitData(const char* data, const int len);
+        /*  Remove a callback by passing in the signal that is supposed
+            to trigger it.
+            Returns: 0 if the callback is successfully removed
+                    -1 if not, i.e. signal doesn't exist              */
+        int removeCallback(int signal);
 
-    /*  Main Loop of BLE handling. Must be called every cycle, as this
-        checks whether the BLE device is receiving data.          */
-    void doBLE();
+        /*  Transmit data through bluetooth. 
+            ## This is a blocking call. ##
+            Returns: 0 on successful addition  
+                                                                      */
+        int transmitData(const char* data, const int len);
+
+        /*  Main Loop of BLE handling. Must be called every cycle, as this
+            checks whether the BLE device is receiving data.          
+            
+            ## This is a blocking call. ##
+
+            If a function that requires an argument to be called is
+            received through bluetooth, then the main loop will be
+            interrupted to wait for the bluetooth command.
+            
+            If the function requires an int, then we expect one value 
+            through bluetooth (uint8_t).
+            If the function requires a float, then we expect two values
+            (uint8_t) x 2. See ints_to_half_float for details on decoding
+                                                                     */
+        void doBLE();
 
 };
 
